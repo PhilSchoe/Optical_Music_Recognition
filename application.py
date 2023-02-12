d0e1836976a4072c59c3f14a8bfd29c39d3156f3
philippschoemacker <p.schoemacker@gmx.de>
2023-02-12 11:48:00 +0100

    Start implementing model
    - Added function to build the model
    - Added test for sparse_tuple_from function, not sure if needed
    - Added some todos and reminders

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 import os
 import cv2 as cv
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
 
 
 def run():
@@ -10,11 +13,40 @@ def run():
     directory_entries = os.listdir(dataset_path)
     batch_size = 10
 
+    # Prepare input data
+
     input_image_batch = prepare_input_images(dataset_path, directory_entries, batch_size)
+
+    # Prepare output data
 
     output_label_batch = prepare_output_labels(dataset_path, directory_entries, batch_size)
 
+    # TODO: Split data into training and validation sets?
+
+    # TODO: Create dataset objects?
+
+    # Build model
+
+    build_model(input_image_batch.shape[2], input_image_batch.shape[1])
+
     print("Application Stopped")
+
+
+def build_model(image_width, image_height):
+    input_image = layers.Input(shape=(image_width, image_height, 1), name="input_image", dtype="float32")
+
+    # First convolutional block: 32 filters; kernel size: 3 x 3; Followed by MaxPooling with window size: 2 x 2
+    # TODO: What about kernel_initializer?
+    x = layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu", padding="same", name="conv0")(input_image)
+    output = layers.MaxPooling2D((2, 2), name="maxPool0")(x)
+
+    model = keras.models.Model(input_image, output, name="OMR_Model")
+
+    optimizer = keras.optimizers.Adam()
+
+    model.compile(optimizer=optimizer)
+
+    model.summary()
 
 
 def prepare_output_labels(dataset_path, directory_entries, batch_size):
@@ -43,7 +75,25 @@ def prepare_output_labels(dataset_path, directory_entries, batch_size):
 
         batch_label_list.append([word_to_int[label] for label in label_list])
 
+    # Test to find out what sparse_tuple_from function from tf-end-to-end gitHub does
+    # targets = sparse_tuple_from(batch_label_list)
+
     return batch_label_list
+
+
+def sparse_tuple_from(sequences, dtype=np.int32):
+    indices = []
+    values = []
+
+    for n, seq in enumerate(sequences):
+        indices.extend(zip([n] * len(seq), range(len(seq))))
+        values.extend(seq)
+
+    indices = np.asarray(indices, dtype=np.int64)
+    values = np.asarray(values, dtype=dtype)
+    shape = np.asarray([len(sequences), np.asarray(indices).max(0)[1] + 1], dtype=np.int64)
+
+    return indices, values, shape
 
 
 def prepare_input_images(dataset_path, directory_entries, batch_size):
@@ -51,8 +101,6 @@ def prepare_input_images(dataset_path, directory_entries, batch_size):
 
     image_height = 180
     image_list = []
-
-    batch_size = 10
 
     for i in range(batch_size):
         entry_name = directory_entries[i]
